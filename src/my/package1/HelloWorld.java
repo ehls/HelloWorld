abfb863d9dedb418da8a69ccb1adede60d3264b6
chen.junguang <920499700@qq.com>
2022-06-23 17:29:56 +0800

    third commit

--- src/my/package1/HelloWorld.java
+++ src/my/package1/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello World!");
         System.out.print("Hello Git!");
+        System.out.print("Hello Git2!");
     }
 }
